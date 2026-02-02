121b33114af24e2c10e878cb6a03d461cec2a6e6
joeswagson <uranousbridge@gmail.com>
2026-02-01 19:06:50 -0800

    YHandler with header offset

--- GENE.Flow/Frontend/src/nodes/YHandler.tsx
+++ GENE.Flow/Frontend/src/nodes/YHandler.tsx
@@ -1,5 +1,5 @@
-export const HANDLE_SPACING = 20;
-export const HANDLE_OFFSET = 40;
+export const HANDLE_SPACING = 24;
+export const HANDLE_OFFSET = 30;
 
-export const handleY = (index: number) =>
-    HANDLE_OFFSET + index * HANDLE_SPACING;
+export const handleY = (index: number, headerHeight: number = 25) =>
+    headerHeight + HANDLE_OFFSET + index * HANDLE_SPACING;
