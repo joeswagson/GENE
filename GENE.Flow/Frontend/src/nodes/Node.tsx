3dfb5be48877c769b21d8b0490f279d3561b056f
joeswagson <uranousbridge@gmail.com>
2026-02-01 19:06:20 -0800

    Fix styles partly

--- GENE.Flow/Frontend/src/nodes/Node.tsx
+++ GENE.Flow/Frontend/src/nodes/Node.tsx
@@ -9,32 +9,46 @@ interface NodeProps {
 }
 
 export const SignalNode = memo(({ data, selected }: NodeProps) => (
-    <>
-        <NodeResizer minWidth={150} minHeight={80} isVisible={selected} />
+    <div className="node-container">
+        <NodeResizer minWidth={150} minHeight={120} isVisible={selected} />
         {Handles(data.signals ?? [], { type: SignalTypes.SIGNAL })}
         <div style={{ padding: 10, minWidth: 150 }}>
             {CreateTitle(data)}
+        </div>
+        <div className="node-signals">
             {Labels(data.signals ?? [])}
         </div>
-    </>
+    </div>
 ));
 
 export const OutputNode = memo(({ data, selected }: NodeProps) => (
-    <>
-        <NodeResizer minWidth={160} minHeight={40} isVisible={selected} />
+    <div className="node-container">
+        <NodeResizer minWidth={160} minHeight={120} isVisible={selected} />
         {Handles(data.outputs ?? [], { type: SignalTypes.OUTPUT })}
         <div style={{ padding: 10, minWidth: 160 }}>
             {CreateTitle(data)}
+        </div>
+        <div className="node-outputs">
             {Labels(data.outputs ?? [])}
         </div>
-    </>
+    </div>
 ));
 
 export const BridgeNode = memo(({ data, selected }: NodeProps) => (
-    <>
-        <NodeResizer minWidth={120} minHeight={60} isVisible={selected} />
+    <div className="bridge-node-container">
+        <NodeResizer minWidth={160} minHeight={120} isVisible={selected} />
         {Handles(data.signals ?? [], { type: SignalTypes.SIGNAL })}
         {Handles(data.outputs ?? [], { type: SignalTypes.OUTPUT })}
         <div style={{ padding: 10, minWidth: 120 }}>{CreateTitle(data)}</div>
-    </>
+        <br/>
+        <div className="flex" style={{alignItems: "baseline", position: "absolute", top: "40px", width: "calc(100% - 20px)", height: "calc(100% - 40px)"}}>
+            <div className="flex-col flex-left node-signals">
+                {Labels(data.signals ?? [])}
+            </div>
+
+            <div className="flex-col flex-right node-outputs">
+                {Labels(data.outputs ?? [])}
+            </div>
+        </div>
+    </div>
 ));
--- GENE.Flow/Frontend/src/nodes/NodeDefaults.tsx
+++ GENE.Flow/Frontend/src/nodes/NodeDefaults.tsx
@@ -2,51 +2,76 @@ import {Handle, Position} from "@xyflow/react";
 import {HandlesData, MethodDefinition, NodeData, SignalType, SignalTypes} from "../types";
 import {handleY} from "./YHandler";
 
-const generalStyle = {}
+const generalStyle = {
+    margin: '0 0',
+    padding: '0 0'
+};
 
 const handleStyle = {
     ...generalStyle
-}
+};
 
 const labelStyle = {
     ...generalStyle
-}
+};
 
 const titleStyle = {
     ...generalStyle,
 
+    position: 'absolute',
+    top: '10px',
     fontSize: 12,
     marginBottom: 8,
     fontWeight: 'bold',
-}
+};
 
 function CreateHandle(method: MethodDefinition,
                       i: number,
                       data: HandlesData) {
     const signal = data.type == SignalTypes.SIGNAL;
     return (<Handle
+
         key={method.name}
+
         id={`${data.type}-${method.name}`}
+
         type={signal ? 'source' : 'target'}
+
         position={method.position ?? signal ? Position.Left : Position.Right}
-        style={{...handleStyle, top: handleY(i)}}
+
+        style={{...handleStyle, top: handleY(i, 25)}}
+
     />);
 }
 
 function CreateLabel(method: MethodDefinition, i: number) {
     return (
-        <div key={method.name} style={{...labelStyle, top: handleY(i)}}>
+
+        <div key={method.name} className="label-item"
+
+        style={{...labelStyle, top: handleY(i, 25)}}
+
+        >
+
             <code>{method.name}</code> ({method.type})
+
         </div>
+
     );
 }
 
 export function CreateTitle(data: NodeData) {
+
     return (
+
         <div style={titleStyle}>
+
             {data.label}
+
         </div>
+
     );
+
 }
 
 export function Handles(methods: MethodDefinition[], data: HandlesData) {
@@ -55,4 +80,5 @@ export function Handles(methods: MethodDefinition[], data: HandlesData) {
 
 export function Labels(methods: MethodDefinition[]) {
     return methods.map((m, i) => CreateLabel(m, i));
+
 }
