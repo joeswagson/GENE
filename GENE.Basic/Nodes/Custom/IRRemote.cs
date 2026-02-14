48b278dbc325686062833a150d9473293b80310a
joeswagson <uranousbridge@gmail.com>
2026-02-14 15:32:47 -0800

    Fix button mapping

--- GENE.Basic/Nodes/Custom/IRRemote.cs
+++ GENE.Basic/Nodes/Custom/IRRemote.cs
@@ -18,15 +18,17 @@ public class IrButtonMapping : ReadOnlyDictionary<string, IrButton>
     public IrButtonMapping(Dictionary<string, IrButton> content) : base(content)
     {
         var buttonArray = Values.ToArray();
-        var width = buttonArray.Select(btn => btn.X).Max();
-        var height = buttonArray.Select(btn => btn.Y).Max();
+        var width = buttonArray.Select(btn => btn.X).Max() + 1;
+        var height = buttonArray.Select(btn => btn.Y).Max() + 1;
 
-        Mapped = new IrButton[][height];
-        for (var row = 0; row <= height; row++)
+        Mapped = new IrButton[height][];
+        for (var row = 0; row < height; row++)
         {
             Mapped[row] = new IrButton[width];
-            for (var col = 0; col <= width; col++)
-                Mapped[row][col] = buttonArray[width * row + col];
+            for (var col = 0; col < width; col++)
+            {
+                Mapped[row][col] = buttonArray.First(b => b.X == col && b.Y == row);
+            }
         }
     }
 
@@ -35,20 +37,37 @@ public class IrButtonMapping : ReadOnlyDictionary<string, IrButton>
     public IrButton this[int x, int y] => Mapped[x][y];
 }
 
-public class IrRemote(string serialPort, IrButtonMapping mapping, string? name = null) : INode
+public class IrRemote(string serialPort, IrButtonMapping mapping, string? name = null) : AsyncNode
 {
-    public string Name => name ?? "IrRemote";
+    public override string Name => name ?? "IrRemote";
 
     public bool IsConnected => Connection is { IsOpen: true };
     public SerialPort? Connection { get; protected set; }
 
-    public virtual void Initialize()
-    {
-        
-        
+    protected override void Initialize()
+    {   
         using var serial = new SerialPort(serialPort, 2000000, Parity.None, 8, StopBits.One);
         serial.Open();
 
         Connection = serial;
+    }
+
+    public event Action<IrButton>? ButtonPressed;
+    protected override async Task Run()
+    {
+        while (true)
+        {
+            if(!IsConnected)
+                await Task.Delay(1000);
+
+            var pairRaw = Connection?.ReadLine();
+            if(pairRaw is null) continue;
+
+            var pair = pairRaw.Split(',');
+            if (!byte.TryParse(pair[0], out var x)) continue;
+            if (!byte.TryParse(pair[1], out var y)) continue;
+            
+            ButtonPressed?.Invoke(new IrButton(x, y));
+        }
     }
 }
