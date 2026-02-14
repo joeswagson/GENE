eceee187c655ea89e4fee9d64cc51e4d1631279b
joeswagson <uranousbridge@gmail.com>
2026-02-14 15:34:18 -0800

    CLI command handler passthrough with program arguments.

--- GENE.CLI/Program.cs
+++ GENE.CLI/Program.cs
@@ -6,19 +6,20 @@ using GENE.Clusters;
 using GENE.JoeRoom;
 using JLogger;
 using System.Text;
-using GENE.Flow;
-using GENE.Flow.Daemon.Protocol.JSON;
-using Spectre.Console;
 using Color = System.Drawing.Color;
 
-namespace GENE.CLI {
-    public static class Meta {
-        public const string VERSION = "1.0.0";
-        public const bool DEBUG = true;
+namespace GENE.CLI
+{
+    public static class Meta
+    {
+        public const string Version = "1.0.0";
+        public const bool Debug = true;
     }
+
     public class Program
     {
         private static CancellationTokenSource AppSource = new();
+
         internal static string[] ParseArgs(string input)
         {
             var args = new List<string>();
@@ -53,6 +54,7 @@ namespace GENE.CLI {
                         args.Add(current.ToString());
                         current.Clear();
                     }
+
                     continue;
                 }
 
@@ -66,16 +68,33 @@ namespace GENE.CLI {
         }
 
 
-        public static readonly Logger logger = new("gene", Color.CornflowerBlue, debug: Meta.DEBUG);
+        public static readonly Logger Logger = new("gene", Color.CornflowerBlue, debug: Meta.Debug);
         public static NodeCluster? CurrentCluster;
-        
+
+        private static object HandleCommand(string identifier, string[] args)
+        {
+            var commandObject = CommandRegistry.GetCommand(identifier);
+            if (commandObject != null)
+            {
+                if (args is not ["?"])
+                    return commandObject.ExecuteInternal(args);
+
+                Logger.Info(commandObject.Help().ToString());
+                return 0;
+            }
+
+            Logger.Error("Command", identifier, "not found.");
+            return 1;
+        }
+
         private static async Task CommandLoop()
         {
             while (!AppSource.IsCancellationRequested)
             {
                 try
                 {
-                    var input = CommandDispatcher.Take(AppSource.Token);
+                    Logger.Info(newline: false, "cmd>");
+                    var input = Console.ReadLine();
 
                     if (string.IsNullOrWhiteSpace(input))
                         continue;
@@ -84,45 +103,31 @@ namespace GENE.CLI {
                     var command = parts[0];
                     var cArgs = parts.Skip(1).ToArray();
 
-                    var commandObject = CommandRegistry.GetCommand(command);
-                    if (commandObject != null)
-                    {
-                        if (cArgs.Length == 1 && cArgs[0] == "?")
-                            logger.Info(commandObject.Help().ToString());
-                        else
-                            commandObject.ExecuteInternal(cArgs);
-                    }
-                    else
-                    {
-                        logger.Error("Command", command, "not found.");
-                    }
+                    HandleCommand(command, cArgs);
                 }
                 catch (Exception e)
                 {
-                    logger.Error($"Error executing command: {e.Message}");
-                    logger.Debug(e.StackTrace ?? "");
+                    Logger.Error($"Error executing command: {e.Message}");
+                    Logger.Debug(e.StackTrace ?? "");
                 }
             }
         }
-        
+
         private static async Task Main(string[] args)
         {
-            Tui.Create();
-            logger.Info("GENE CLI");
-            logger.Info("- General Environment Node Engine");
-            logger.Info("- GENE Version:", Gene.Meta.VERSION);
-            logger.Info("- GENE CLI Version:", Meta.VERSION);
-            logger.Newline();
+            if (args.Length > 0)
+            {
+                CommandRegistry.RegisterCommands();
+            }
+
+            Logger.Info("GENE CLI");
+            Logger.Info("- General Environment Node Engine");
+            Logger.Info("- GENE Version:",     Gene.Meta.Version);
+            Logger.Info("- GENE CLI Version:", Meta.Version);
+            Logger.Newline();
 
             CommandRegistry.RegisterCommands();
-
-            logger.Info("Initializing Flow...");
-            
-            var pat = args[0];
-            CurrentCluster = new RoomCluster(pat);
-
             _ = Task.Run(CommandLoop);
-            Tui.Run();
             await Task.Delay(Timeout.Infinite);
             //var action = SmartActions.Light(light.Name, pat);
 
