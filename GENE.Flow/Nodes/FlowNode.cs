d0cc4415777a7e0278f16fd54e572e1814bc8400
joeswagson <uranousbridge@gmail.com>
2026-02-15 18:01:03 -0800

    Handle-members for flownode type.

--- GENE.Flow/Nodes/FlowNode.cs
+++ GENE.Flow/Nodes/FlowNode.cs
@@ -1,12 +1,33 @@
+using System.Net.Http.Headers;
 using GENE.Flow.Clusters;
 using GENE.Nodes;
 
 namespace GENE.Flow.Nodes;
 
+public enum SignalType
+{
+    Signal, Output
+}
 public record FlowNode(FlowCluster Cluster, int Index, string NodeId, INode Node)
 {
-    public List<FlowEdge> Signals = [];
-    public List<FlowEdge> Outputs = [];
+    public List<FlowHandle> Signals = [];
+    public List<FlowHandle> Outputs = [];
+
+    public void AddHandle(SignalType type, FlowHandle handle)
+    {
+
+        switch (type)
+        {
+            case SignalType.Signal:
+                Signals.Add(handle);
+                break;
+            case SignalType.Output:
+                Outputs.Add(handle);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(type), type, null);
+        }
+    }
 
     public void Signalled(SignalActivation signal)
     {
@@ -19,7 +40,7 @@ public record FlowNode(FlowCluster Cluster, int Index, string NodeId, INode Node
     }
 }
 
-public record FlowNode<TP, TR>(FlowCluster Cluster, int Index, string NodeId, INode<TP, TR> Source) 
-    : FlowNode(Cluster, Index, NodeId, Source)
-    where TP : NodePayload
-    where TR : NodeResponse;
+// public record FlowNode<TP, TR>(FlowCluster Cluster, int Index, string NodeId, INode<TP, TR> Source) 
+//     : FlowNode(Cluster, Index, NodeId, Source)
+//     where TP : NodePayload
+//     where TR : NodeResponse;
