949270428693e8ba95b0111862ad3e1fe7a08909
joeswagson <uranousbridge@gmail.com>
2026-02-16 13:50:37 -0800

    Log route choices instead of ugly error in CommandWrapper.cs

--- GENE.CLI/Commands/Types/CommandWrapper.cs
+++ GENE.CLI/Commands/Types/CommandWrapper.cs
@@ -11,8 +11,13 @@ public abstract class CommandWrapper<T> : Command where T : notnull
     public override object Execute(string[] args)
     {
         var route = Argument<T>(0);
-        return Routes.TryGetValue(route, out var cmd)
-            ? cmd.ExecuteInternal(args.Skip(1).ToArray())
-            : throw new ArgumentOutOfRangeException(nameof(args), $"Route {route} was invalid.");
+        var routedArgs = args.Skip(1).ToArray();
+        
+        if (route is not null && Routes.TryGetValue(route, out var command)) 
+            return command.ExecuteInternal(routedArgs);
+        
+        // route not found
+        var keysJoined = string.Join(", ", Routes.Keys);
+        return new Exception($"Invalid route. Choices: {keysJoined}");
     }
 }
